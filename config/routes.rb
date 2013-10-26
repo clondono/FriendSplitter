8b6c49a361e46b97fde3779e67d415a129efd765
Angel Batista <angel.batista1992@gmail.com>
2013-10-25 22:59:28 -0400

    Added resources to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,13 @@ Friendsplitter::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-   root 'home#index'
+  root 'home#index'
+
+
+  resources :debts, only: [:create, :destroy]
+  resources :contribution, only: [:create, :destroy]
+  resources :events
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
