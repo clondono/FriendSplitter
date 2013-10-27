93dea88e7b592c95d338c447d0a1a1a012c1dbe2
Lucy Yu <lucyyu@mit.edu>
2013-10-27 17:48:23 -0400

    Start making an Event detail page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,55 +1,59 @@
 class EventsController < ApplicationController	
-	def index
-  	end
-
-  def new
-    @event = Event.new
-    1.times {@event.contributions.build}    
-  end
-  
- 	def create
-    # Extract contribution attributes from params
-    # and rebuild the params hash.
-    contributionsInfo = event_params.slice(:contributions_attributes)
-    title = event_params.slice(:title)
-    desc = event_params.slice(:description)
-    amount = event_params.slice(:amount)
-    newParams = title.merge(desc).merge(amount)
-    
-    # Create the event.
-    @event = Event.new(newParams)
-
-    #TODO check if valid contribution amounts.
-    # Create method in model and call it...
-
-    # Save event and redirect appropriately.
-    if @event.save
-      # Create contributions and debts.
-      @event.createContributions(contributionsInfo)
-      @event.createDebts
-
-      flash[:success] = "Event Created"
-      redirect_to root_url
-    else
-      render 'new'
+    def index
     end
 
-  end
+    def show
+        @event = Event.find_by_id(params[:id])
+    end
 
-	def destroy
-	end
+    def new
+        @event = Event.new
+        1.times {@event.contributions.build}    
+    end
 
-	def update
-  end
+    def create
+        # Extract contribution attributes from params
+        # and rebuild the params hash.
+        contributionsInfo = event_params.slice(:contributions_attributes)
+        title = event_params.slice(:title)
+        desc = event_params.slice(:description)
+        amount = event_params.slice(:amount)
+        newParams = title.merge(desc).merge(amount)
+
+        # Create the event.
+        @event = Event.new(newParams)
+
+        #TODO check if valid contribution amounts.
+        # Create method in model and call it...
+
+        # Save event and redirect appropriately.
+        if @event.save
+            # Create contributions and debts.
+            @event.createContributions(contributionsInfo)
+            @event.createDebts
+
+            flash[:success] = "Event Created"
+            redirect_to root_url
+        else
+            render 'new'
+        end
+
+    end
+
+    def destroy
+    end
+
+    def update
+    end
 
 
 
 
-  private
+    private
     # Strong parameters for security (rails way)
     def event_params
-      params.require(:event).permit(:title, 
-        :description, :amount, contributions_attributes:[:email,:amount,:paid])
+        params.require(:event).permit(:title, 
+                                      :description, :amount, contributions_attributes:[:email,:amount,:paid])
     end
 
 end
