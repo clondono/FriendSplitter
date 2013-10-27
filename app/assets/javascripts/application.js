b15cdd69ab70ad0e2befcff58e52c467f12f947c
Lucy Yu <lucyyu@mit.edu>
2013-10-26 23:04:46 -0400

    layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
 //= require turbolinks
 //= require_tree .
-//= bootstrap
 
 
 // Used to delete participants from a Event Creation Form
