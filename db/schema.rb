18aa5e84c112b75c598fdb4ee0d82a061543bc4f
Christian Londono <clondono@clondono.(none)>
2013-11-02 15:12:32 -0400

    cleaned up user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131102094645) do
+ActiveRecord::Schema.define(version: 20131102191028) do
 
   create_table "authentications", force: true do |t|
     t.string   "provider"
@@ -69,9 +69,6 @@ ActiveRecord::Schema.define(version: 20131102094645) do
     t.datetime "updated_at"
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
