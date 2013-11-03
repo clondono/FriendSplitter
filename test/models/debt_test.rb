aa37eeda792cbb1e92f935935d686ba205614685
Lucy Yu <lucyyu@mit.edu>
2013-11-03 15:54:34 -0500

    Add more tests

--- test/models/debt_test.rb
+++ test/models/debt_test.rb
@@ -16,6 +16,36 @@ class DebtTest < ActiveSupport::TestCase
         assert( debt1.amount == 8.20 , "Debt value after update should be 8.20")
     end
 
+    test "Debt shouldn't save if not unique" do
+        debt = Debt.new
+        debt.owner_id = 3
+        debt.amount = 3.00
+        debt.indebted_id = 2
+        assert !debt.save, "Debt should not save if not unique"
+    end
+
+    test "Debt shouldn't save if owner_id not present" do
+        debt = Debt.new
+        debt.amount = 3.00
+        debt.indebted_id = 2
+        assert !debt.save, "Debt should not save if owner_id not present"
+    end
+
+    test "Debt shouldn't save if amount not present" do
+        debt = Debt.new
+        debt.owner_id = 1
+        debt.indebted_id = 2
+        assert !debt.save, "Debt should not save if owner_id not present"
+    end
+
+    test "Debt should save if valid fields" do
+        debt = Debt.new
+        debt.owner_id = 1
+        debt.indebted_id = 2
+        debt.amount = 2.40
+        assert debt.save, "Debt should save if owner_id, indebted_id, amount are all present"
+    end
+
 
 
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -11,17 +11,10 @@ class UserTest < ActiveSupport::TestCase
 
     test "Confirm debt exists" do
         owner = users(:three)
-        Rails.logger.debug("owner id: #{owner.id}")
         indebted = users(:two)
-        Rails.logger.debug("indebted id: #{indebted.id}")
-        find = Debt.all
-
-        find.each do |f|
-            Rails.logger.debug("f owner: #{f.owner_id}")
-            Rails.logger.debug("f indebted: #{f.indebted_id}")
-        end
-
         assert( owner.owes?(indebted), "debt object states user two owes user one")
     end
+
+
 
 end
